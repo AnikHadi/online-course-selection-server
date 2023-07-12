156f5a045de19448e53a636598723523514bced1
HadiAnik <hadiuzzamananik@gmail.com>
2023-07-12 14:05:54 +0600

    update

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const database = async () => {
   try {
     const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster0.17kyq.mongodb.net/learning_portal?retryWrites=true&w=majority`;
     // const uri = "mongodb://127.0.0.1:27017/learning_portal";
-    const mongo = await mongoose.connect(uri);
+    const mongo = mongoose.connect(uri);
     if (mongo) {
       console.log("_________MongoDB successfully connected_________");
     }
